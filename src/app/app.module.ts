67566fa27140268d06370f21c20fdff49411ee0a
copa777 <copadalessandro@gmail.com>
2025-05-28 10:36:19 -0400

    arreglo del boton de volver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,26 +12,32 @@ import { CommonModule } from '@angular/common';
 //Modulos
 import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ProveedorComponent } from './proveedor/proveedor.component';
 import { LayoutComponent } from './layout/layout.component';
+import { AlmacenesComponent } from './almacenes/almacenes.component';
+import { ProductoComponent } from './producto/producto.component';
+import { InventarioComponent } from './inventario/inventario.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { SignInComponent } from './new-User/sign-in.component';
 import { NewPasswordComponent } from './new-password/new-password.component';
-import { ProductoComponent } from './producto/producto.component';
-import { BitacoraComponent } from './bitacora/bitacora.component';
+
 import { AddNewProveedorComponent } from './add-new-proveedor/add-new-proveedor.component';
+import { AddNewAlmacenComponent } from './add-new-almacen/add-new-almacen.component';
+import { BitacoraComponent } from './bitacora/bitacora.component';
+
+import { NewInventarioComponent } from './new-inventario/new-inventario.component';
+
 import { SessionLostComponent } from './session-lost/session-lost.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { UserPermisosComponent } from './user-permisos/user-permisos.component';
 import { ComprarComponent } from './comprar/comprar.component';
 import { NewCompraComponent } from './new-compra/new-compra.component';
 import { BoletaCompraComponent } from './boleta-compra/boleta-compra.component';
-import { NewInventarioComponent } from './new-inventario/new-inventario.component';
-import { AddNewAlmacenComponent } from './add-new-almacen/add-new-almacen.component';
-import { InventarioComponent } from './inventario/inventario.component';
-import { AlmacenesComponent } from './almacenes/almacenes.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,21 +45,22 @@ import { AlmacenesComponent } from './almacenes/almacenes.component';
     MenuComponent,
     ProveedorComponent,
     LayoutComponent,
+    AlmacenesComponent,
+    ProductoComponent,
+    InventarioComponent,
     SpinnerComponent,
     SignInComponent,
     NewPasswordComponent,
     AddNewProveedorComponent,
+    AddNewAlmacenComponent,
+    BitacoraComponent,
+    NewInventarioComponent,
     SessionLostComponent,
     UsuarioComponent,
-    UserPermisosComponent,  
-    ProductoComponent,
-    BitacoraComponent,
-    AlmacenesComponent,
-    InventarioComponent,
-    AddNewAlmacenComponent,
-    NewInventarioComponent,
+    UserPermisosComponent,
     ComprarComponent,
     NewCompraComponent,
+    BoletaCompraComponent, 
   ],
   imports: [
     BrowserModule,
